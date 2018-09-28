a9204849ed17ad48d2f344d5247f0514939fec75
PlumStream <zhangjian19870523@126.com>
2018-09-28 09:23:58 +0800

    test

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import tensorflow as tf
 sess = tf.InteractiveSession()
 x = tf.constant([[2,5,3,-5],[0,3,-2,5],[4,3,5,3],[6,1,4,0]])
 y = tf.shape(x)
-
+#teattataata afaf
+#teatat
 print  sess.run(y)
